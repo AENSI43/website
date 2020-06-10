c9dde10df3e11a6e047e933fab4f4be6b6a3119c
Killian Mahé <killianmahe.pro@gmail.com>
2020-06-10 19:07:31 +0200

    ✅ Adding Active Character Tests

--- tests/Feature/CharacterTest.php
+++ tests/Feature/CharacterTest.php
@@ -5,19 +5,34 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-use App\Model\Character;
+use App\Models\Character;
+use App\User;
 
 class CharacterTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    public function testGetActiveCharacter()
+    {
+        $user = factory(User::class)->create();
+
+        $character = factory(Character::class)->create();
+
+        // Assert it works when there is an active character
+
+        $user->characters()->save($character);
+
+        $response = $this->actingAs($user, 'api')->getJson('/api/character/active-character');
+
+        $response->assertOk();
+
+        // Assert it works when there is no active character
+
+        $character->active = false;
+        $character->save();
+
+        $response = $this->actingAs($user, 'api')->getJson('/api/character/active-character');
+
+        $response->assertOk();
     }
 }
